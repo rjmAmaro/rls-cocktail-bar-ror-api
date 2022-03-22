83c50b61e9ec06499b31095a9da71107c27a5851
jRocha494 <jmarocha494@gmail.com>
2022-03-22 23:21:01 +0000

    API_04 & API_05 upgrades (still not tested)
    Lists all cocktails and ingredients without being dependent on other Models ids

--- api/app/controllers/api/cocktails_controller.rb
+++ api/app/controllers/api/cocktails_controller.rb
@@ -4,6 +4,11 @@ require 'json'
 module Api
   class CocktailsController < ApplicationController
     def index
+      cocktails = Cocktail.where(category_id: params[:category_id])
+      render json: {cocktails: cocktails}
+    end
+
+    def list_all
       cocktails = Cocktail.order('name')
       render json: {cocktails: cocktails}
     end
@@ -14,7 +19,7 @@ module Api
         render json: {cocktail: nil}
       else
         cocktail_category = Category.find_by(id: cocktail.category_id).name
-        render json: {name: cocktail.name, desc: cocktail.desc, category: cocktail_category, photo: cocktail.photo}
+        render json: {name: cocktail.name, desc: cocktail.desc, category: cocktail_category, photo: cocktail.photo, ingredients: cocktail.ingredients}
       end
     end
   end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,10 +4,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   namespace 'api' do
-    resources :categories do 
+    resources :categories do
       resources :cocktails do
+        collection do
+
+        end
         resources :ingredients
       end
     end
+
+    #Routes that are not dependent on other Models Ids
+    get '/cocktails/all', to: 'cocktails#list_all'
+    get '/ingredients/all', to: 'ingredients#list_all'
   end
 end
